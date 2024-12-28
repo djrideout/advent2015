904b6f44fd72cc3c3029c41c42d3322805ee233b
D.J. Rideout <djrideoutgit@proton.me>
2024-12-28 02:21:15 +0000

    Day 7, part 2

--- day7.py
+++ day7.py
@@ -5,7 +5,8 @@ async def eval_gate(str: str, wires):
     left, out = str.split(" -> ")
     inputs = left.split(" ")
     if len(inputs) == 1:
-        wires[out].set_result(int(inputs[0]) if inputs[0].isdigit() else await wires[inputs[0]])
+        if not wires[out].done():
+            wires[out].set_result(int(inputs[0]) if inputs[0].isdigit() else await wires[inputs[0]])
     elif len(inputs) == 2:
         wires[out].set_result(~await wires[inputs[1]])
     else:
@@ -18,16 +19,23 @@ async def eval_gate(str: str, wires):
             case "LSHIFT": wires[out].set_result(a << b)
             case "RSHIFT": wires[out].set_result(a >> b)
 
-async def main(loop):
-    input = open("input/day7.txt").readlines()
+async def get_wire_a_value(input, defaults):
     wires = defaultdict(lambda: loop.create_future())
+    for key in defaults:
+        wires[key].set_result(defaults[key])
     evals = []
     for l in input:
         evals.append(eval_gate(l.rstrip(), wires))
     await asyncio.gather(*evals)
-    print(wires["a"].result())
+    return wires["a"].result()
+
+async def main():
+    input = open("input/day7.txt").readlines()
+    a = await get_wire_a_value(input, {})
+    print(a)
+    print(await get_wire_a_value(input, { "b": a }))
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.run_until_complete(main(loop))
+    loop.run_until_complete(main())
